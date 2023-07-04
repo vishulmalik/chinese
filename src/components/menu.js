7f96b5f7629b52fe71cb04d273cbe4248b9fbac3
Vishul Malik <vishul@appsnranks.com>
2023-07-04 12:16:00 +0000

    menu

---
+++ src/components/menu.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const MainMenu = () => {
+  const { menu } = useStaticQuery(graphql`
+    query menus {
+    nodes {
+      id
+      databaseId
+      name
+      menuItems {
+        edges {
+          node {
+            id
+            label
+            parentId
+            url
+          }
+        }
+      }
+    }
+  }
+  
+ return (
+   <nav>
+   <ul>
+   <li>Main Menu</li>
+   </ul>
+   </nav>
+  )
+}
+
+export default Menu
+
