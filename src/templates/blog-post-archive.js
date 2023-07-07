0aa71a4cde772afc43311e4dba37ed2be4ff8b61
Vishul Malik <vishul@appsnranks.com>
2023-07-07 11:31:53 +0000

    menu

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -5,7 +5,6 @@ import parse from "html-react-parser"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import HeaderMenu from "../components/menu"
 
 const BlogIndex = ({
   data,
@@ -31,7 +30,6 @@ const BlogIndex = ({
     <Layout isHomePage>
 
       <Seo title="All posts" />
-<HeaderMenu />
       <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
